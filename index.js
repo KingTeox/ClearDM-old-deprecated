c1b86793fe955648aedaffd62be8d4ef35ebdefc
Carlos Eduardo <65124135+KingTeox@users.noreply.github.com>
2022-04-27 12:23:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,25 +8,24 @@ Client.on("ready", async () => {
 
     /**
      * 
-     * @param {String} id 
      * @param {String} idDM 
      */
     async function Clear(idDM) {
         
-        console.log(`[Teox] <ClearFunction> Started.`);
+        console.log(`[Teox] <ClearFunction> Started and Working.`);
 
         let ClearMembro = Client.users.cache.get(idDM);
         let DM = await ClearMembro.send("[CLEAR]");
         let Messages = await DM.channel.messages.fetch();
         let Filtro = Messages.filter(m => m.author.id === Client.user.id);
         
-        console.log(`[Teox] <ClearFunction> Messages ${Filtro.size} deleting.`);
+        console.log(`[Teox] <ClearFunction> Deleting ${Filtro.size} Messages.`);
 
         function Delete() {
             let deleteMSG = Filtro.random();
             deleteMSG.delete().then(Deleted => {
                 Filtro.delete(Filtro.id);
-                console.log(`[Teox] <ClearFunction> Message deleted [${Deleted.content}]`);
+                console.log(`[Teox] <ClearFunction> Message [${Deleted.content}] Deleted :)`);
             }).catch(Err => { return; });
         }; 
         setInterval(() => { 
